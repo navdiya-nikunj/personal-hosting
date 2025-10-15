c2b35fa6decb379028304e9f3e4dc221c3772503
navdiya-nikunj <nikunjmnavdiya@gmail.com>
2025-10-16 01:48:01 +0530

    Analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Suspense } from "react";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "Document Hosting Platform",
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className="antialiased">
         <Suspense fallback={<div>Loading...</div>}> 
         {children}
+        <Analytics />
         </Suspense>
       </body>
     </html>
